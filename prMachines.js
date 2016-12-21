ca39ea4fa151441a4627111f1dff5107c4280f2b
bernardio5 <mcdonald107@gmail.com>
2016-12-21 07:46:09 -0500

    pentagon
    pentagon machine

--- prMachines.js
+++ prMachines.js
@@ -42,14 +42,14 @@ prMachines.prototype = {
 		var p2 = inArray["p2"];
 		var pAbove = inArray["pAbove"];  // input error checking?
 
-		var c1 = this.pg.addCircle(p1, p2); 
-		var c2 = this.pg.addCircle(p2, p1);
+		var c1 = this.pg.circle(p1, p2); 
+		var c2 = this.pg.circle(p2, p1);
 
-		var int1 = this.pg.addFirstIntersection(c1, c2, pAbove); 
-		var int2 = this.pg.addSecondIntersection(c1, c2, pAbove); 
-		var midLine = this.pg.addLine(int1, int2);
-		var conr = this.pg.addLine(p1, p2); 
-		var midpt = this.pg.addFirstIntersection(midLine, conr, pAbove);   
+		var int1 = this.pg.first(c1, c2, pAbove); 
+		var int2 = this.pg.second(c1, c2, pAbove); 
+		var midLine = this.pg.line(int1, int2);
+		var conr = this.pg.line(p1, p2); 
+		var midpt = this.pg.first(midLine, conr, pAbove);   
 
 		var result = {"MidPoint":midpt, "MidLine":midLine, "Connector":conr, "I1":int1, "I2":int2, "C1":c1, "C2":c2};
 		return result;
@@ -76,9 +76,9 @@ prMachines.prototype = {
 		var p2 = inArray["p2"];
 		var pAbove = inArray["pAbove"];
 
-		var c1 = this.pg.addCircle(p1, p2); 
-		var conector = this.pg.addLine(p1, p2);
-		var p3 = this.pg.addSecondIntersection(conector, c1, p2); 
+		var c1 = this.pg.circle(p1, p2); 
+		var conector = this.pg.line(p1, p2);
+		var p3 = this.pg.second(conector, c1, p2); 
 		var diag = this.midpoint({"p1":p3, "p2":p2, "pAbove":pAbove});
 
 		var result = {"Perpendicular":diag["MidLine"], 
@@ -114,7 +114,7 @@ prMachines.prototype = {
 		var t = inArray["t"];
 		var i; 
 
-		var c1 = this.pg.addCircle(p1, p2);
+		var c1 = this.pg.circle(p1, p2);
 		var Ci, Pi, Pim, Pip, Cname, Pname; 
 		Pim = p1; // p(i-1)
 		Pi = p2; // pi
@@ -122,8 +122,8 @@ prMachines.prototype = {
 		for (i=2; i<(n+1); i=i+1) { 
 			Cname = "C" + i; 
 			Pname = "P" + i; 
-			Ci = this.pg.addCircle(Pi, Pim);
-			Pip = this.pg.addSecondIntersection(t, Ci, Pim);  // p(i+1)=Pip
+			Ci = this.pg.circle(Pi, Pim);
+			Pip = this.pg.second(t, Ci, Pim);  // p(i+1)=Pip
 			result[Pname] = Pi;
 			result[Cname] = Ci;
 			Pim = Pi; 
@@ -134,9 +134,9 @@ prMachines.prototype = {
 	series_test: function() { 
 		var p1 = this.pg.given(0.25, 0.5);
 		var p2 = this.pg.given(0.55, 0.5);
-		var l1 = this.pg.addLine(p1, p2);
+		var l1 = this.pg.line(p1, p2);
 		var md = this.series(10, {"p1":p1, "p2":p2, "t":l1});
-		var c1 = this.pg.addCircle(p1, p2);
+		var c1 = this.pg.circle(p1, p2);
 		var md = this.series(10, {"p1":p1, "p2":p2, "t":c1});
 	},
 
@@ -156,20 +156,20 @@ prMachines.prototype = {
 		var p12 = inArray["p2"];
 		var pAbove = inArray["pAbove"];
 
-		var c0 = this.pg.addCircle(p0, p12); 
-		var c12 = this.pg.addCircle(p12, p0); 
-		var p2 = this.pg.addFirstIntersection(c0, c12, pAbove);
-		var p10 = this.pg.addSecondIntersection(c0, c12, pAbove);
+		var c0 = this.pg.circle(p0, p12); 
+		var c12 = this.pg.circle(p12, p0); 
+		var p2 = this.pg.first(c0, c12, pAbove);
+		var p10 = this.pg.second(c0, c12, pAbove);
 
-		var c2 = this.pg.addCircle(p2, p12); 
-		var c10 = this.pg.addCircle(p10, p12); 
-		var p4 = this.pg.addSecondIntersection(c0, c2, p12);
-		var p8 = this.pg.addSecondIntersection(c0, c10, p12);
+		var c2 = this.pg.circle(p2, p12); 
+		var c10 = this.pg.circle(p10, p12); 
+		var p4 = this.pg.second(c0, c2, p12);
+		var p8 = this.pg.second(c0, c10, p12);
 
-		var c4 = this.pg.addCircle(p4, p2); 
-		var c8 = this.pg.addCircle(p8, p10); 
-		var p6 = this.pg.addSecondIntersection(c4, c8, p0);
-		var c6 = this.pg.addCircle(p6, p4);
+		var c4 = this.pg.circle(p4, p2); 
+		var c8 = this.pg.circle(p8, p10); 
+		var p6 = this.pg.second(c4, c8, p0);
+		var c6 = this.pg.circle(p6, p4);
 
 		var result = {
 				"p2":p2, "c2":c2,  "p4":p4, "c4":c4,  "p6":p6, "c6":c6, 
@@ -187,31 +187,93 @@ prMachines.prototype = {
 
 
 
+
+
+
+	pentagonInCircle: function(inArray) {
+		var p0 = inArray["p1"];
+		var p360 = inArray["p2"];
+		var pAbove = inArray["pAbove"];
+
+		var c0 = this.pg.circle(p0, p360); 
+		var ln360 = this.pg.line(p0, p360);
+		var p180 = this.pg.second(c0, ln360, pAbove);
+
+		var c360_2 = this.pg.circle(p360, p180); 
+		var c180_2 = this.pg.circle(p180, p360); 
+
+		var p90_2 = this.pg.first(c360_2, c180_2, pAbove);
+		var p270_2 = this.pg.second(c360_2, c180_2, pAbove);
+		var equator = this.pg.line(p90_2, p270_2);
+
+		var p90 = this.pg.first(equator, c0, pAbove); 
+		var p270 = this.pg.second(equator, c0, pAbove); 
+
+		var c90 = this.pg.circle(p90, p0); 
+		var pMidsHi = this.pg.first(c90,c0, pAbove); 
+		var pMidsLo = this.pg.second(c90,c0, pAbove); 
+
+		var lnMids = this.pg.line(pMidsHi, pMidsLo); 
+
+		var pHalfRadius = this.pg.first(lnMids, equator, pAbove);
+		var cA = this.pg.circle(pHalfRadius, p360);
+		var pPent = this.pg.first(equator, cA, p270);
+
+		var cPent360 = this.pg.circle(p360, pPent); 
+		var  p72 = this.pg.first(c0, cPent360, p90); 
+		var p288 = this.pg.first(c0, cPent360, p270); 
+		var  c72 = this.pg.circle( p72, p360); 
+		var c288 = this.pg.circle(p288, p360); 
+
+		var p144 = this.pg.second(c0, c72, p360);
+		var p216 = this.pg.second(c0, c288, p360);
+
+		var ln360 = this.pg.line(p360, p72); 
+		var  ln72 = this.pg.line(p72,  p144); 
+		var ln144 = this.pg.line(p144, p216); 
+		var ln216 = this.pg.line(p216, p288); 
+		var ln288 = this.pg.line(p288, p360); 
+
+		// verification: the lines make a star with points touching cTest
+		var pTest = this.pg.first(ln360, ln144, pAbove);
+		var cTest = this.pg.circle(p0,pTest); 
+
+
+	},
+	pentagonInCircle_test: function() { 
+		var p1 = this.pg.given(0.6, 0.5);
+		var p2 = this.pg.given(0.43, 0.43);
+		var pAbove = this.pg.given(0.5, 0.1);
+		var md = this.pentagonInCircle({"p1":p1, "p2":p2, "pAbove":pAbove});
+	},
+
+
+
 	// this is incorrect; gives two ("vescia?"); not using center 
 	flower4: function(inArray) {
 		var p0 = inArray["p1"];
 		var p12 = inArray["p2"];
 		var pAbove = inArray["pAbove"];
 
-		var c0 = this.pg.addCircle(p0, p12); 
-		var c12 = this.pg.addCircle(p12, p0); 
-		var l1 = this.pg.addLine(p0,p12); 
-		var p6 = this.pg.addSecondIntersection(c0,l1, p0); 
+		var c0 = this.pg.circle(p0, p12); 
+		var c12 = this.pg.circle(p12, p0); 
+		var l1 = this.pg.line(p0,p12); 
+		var p6 = this.pg.second(c0,l1, p0); 
 
-		var c6cr = this.pg.addCircle(p6, p12); 
-		var c12cr = this.pg.addCircle(p12, p6);
-		var p3cr = this.pg.addFirstIntersection(c6cr, c12cr, pAbove); 
-		var p9cr = this.pg.addSecondIntersection(c6cr, c12cr, pAbove); 
-		var crossLine = this.pg.addLine(p3cr, p9cr); 
+		var c6cr = this.pg.circle(p6, p12); 
+		var c12cr = this.pg.circle(p12, p6);
+		var p3cr = this.pg.first(c6cr, c12cr, pAbove); 
+		var p9cr = this.pg.second(c6cr, c12cr, pAbove); 
+		var crossLine = this.pg.line(p3cr, p9cr); 
 
 
-		var p3 = this.pg.addFirstIntersection(c0, crossLine, pAbove); 
-		var p9 = this.pg.addSecondIntersection(c0, crossLine, pAbove); 
+		var p3 = this.pg.first(c0, crossLine, pAbove); 
+		var p9 = this.pg.second(c0, crossLine, pAbove); 
 
-		c12 = this.pg.addCircle(p12, p3); 
-		c3 = this.pg.addCircle(p3, p6); 
-		c6 = this.pg.addCircle(p6, p9); 
-		c9 = this.pg.addCircle(p9,p12); 
+		c12 = this.pg.circle(p12, p3); 
+		c3 = this.pg.circle(p3, p6); 
+		c6 = this.pg.circle(p6, p9); 
+		c9 = this.pg.circle(p9,p12); 
 		// yeah, this is kind of an eyeball; don't think so. 
 		var result = {
 				"p3":p3, "p6":p6, "p9":p9, "p12":p12, 
@@ -251,10 +313,10 @@ prMachines.prototype = {
 		var midPt = mids["MidPoint"];
 		var c1 = this.pg.circle(p1,p2);
 		var c2 = this.pg.circle(midPt, p1); // circle at p1 through p3
-		var pt1 = this.pg.addFirstIntersection(c1, c2, pAbove);
-		var pt2 = this.pg.addSecondIntersection(c1, c2, pAbove);
-		var l1 = this.pg.addLine(p3,pt1);
-		var l2 = this.pg.addLine(p3,pt2);
+		var pt1 = this.pg.first(c1, c2, pAbove);
+		var pt2 = this.pg.second(c1, c2, pAbove);
+		var l1 = this.pg.line(p3,pt1);
+		var l2 = this.pg.line(p3,pt2);
 		var result = {"L1":l1, "L2":l2, "PT1":pt1, "PT2":pt2}; 
 		return result;
 	},
@@ -298,6 +360,8 @@ prMachines.prototype = {
 
 
 	// given two points defining a circle, build a n-gon within the circle (for n=3,4,5,6,7,8,9)
+
+
 	// given two points, build an n-gon that has a side given by the points
 	// given an n-gon, draw a circle inside/outside, return the ith side, 
 	// given two points, give an ordered set of rectilinear grid points
@@ -318,25 +382,25 @@ prMachines.prototype = {
 		p1 = this.addGivenPoint(xin, yin);     		// 1: origin
 		p2 = this.addGivenPoint(xin+win, yin);   	// 2: pt at origin+(w,0)
 		p3 = this.addGivenPoint(xin+hin, yin);   	// 3: pt at origin+(h,0);
-		l1 = this.addLine(p1, p2);               	// 4: top margin
-		c1 = this.addCircle(p1, p2);             	// 5
-		c2 = this.addCircle(p1, p3);             	// 6
-		p4 = this.addSecondIntersection(c1, l1, p2);// 7
-		c3 = this.addCircle(p4, p2);             	// 8
-		c4 = this.addCircle(p2, p4);             	// 9
-		p5 = this.addSecondIntersection(c3, c4, p1);//10:
-		l2 = this.addLine(p1, p5);               	//11: left margin
-		p6 = this.addSecondIntersection(c1, l2, p4);    //12: bottom-left of square
-		p7 = this.addFirstIntersection(c2, l2, p6);    //13: bottom-left of page
-		c5 = this.addCircle(p2, p1);             	//14
-		c6 = this.addCircle(p6, p1);             	//15
-		p8 = this.addFirstIntersection(c5, c6, p5); //16: bottom-right of square
-		l3 = this.addLine(p2, p8);                	//17: right margin
-		c7 = this.addCircle(p3, p1);             	//18
-		c8 = this.addCircle(p7, p1);             	//19
-		p9 = this.addSecondIntersection(c7, c8, p1); //20:
-		l4 = this.addLine(p7, p9);                	//21: bottom margin
-		p10= this.addFirstIntersection(l4, l3, p9); //22: bottom-right of page
+		l1 = this.line(p1, p2);               	// 4: top margin
+		c1 = this.circle(p1, p2);             	// 5
+		c2 = this.circle(p1, p3);             	// 6
+		p4 = this.second(c1, l1, p2);// 7
+		c3 = this.circle(p4, p2);             	// 8
+		c4 = this.circle(p2, p4);             	// 9
+		p5 = this.second(c3, c4, p1);//10:
+		l2 = this.line(p1, p5);               	//11: left margin
+		p6 = this.second(c1, l2, p4);    //12: bottom-left of square
+		p7 = this.first(c2, l2, p6);    //13: bottom-left of page
+		c5 = this.circle(p2, p1);             	//14
+		c6 = this.circle(p6, p1);             	//15
+		p8 = this.first(c5, c6, p5); //16: bottom-right of square
+		l3 = this.line(p2, p8);                	//17: right margin
+		c7 = this.circle(p3, p1);             	//18
+		c8 = this.circle(p7, p1);             	//19
+		p9 = this.second(c7, c8, p1); //20:
+		l4 = this.line(p7, p9);                	//21: bottom margin
+		p10= this.first(l4, l3, p9); //22: bottom-right of page
 	}
 
 
